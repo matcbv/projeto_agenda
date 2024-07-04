9af4352ec760041c4404d50accaa3bb4eb8c0935
matcbv <matheuscbv23@gmail.com>
2024-07-04 20:35:03 -0300

    Correcao do caminho publico estatico

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'public', 'assets'),
         filename: "js/bundle.js",
-        publicPath: "public/assets/"
+        publicPath: "/assets/"
     }, 
     module: {
         rules: [
